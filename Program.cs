35099b82375edb05b27aaccda059dddf11ce96c1
luftwaffe1985 <putik1985@yandex.ru>
2024-04-01 20:17:17 +0300

    new change

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Write("Введите первое число и нажмите клавишу Enter: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число и нажмите клавишу Enter: ");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("Максимальное число " + a);
+}
+else
+{
+    Console.WriteLine("Максимальное число " + b);
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # GIT_Deep_HW_1
-# This is the first lesson 
+
+# This is the first lesson
+
+# Making changes
